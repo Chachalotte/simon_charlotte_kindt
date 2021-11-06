1c8cf0e2602e66feadc82e9f774e188dc240368f
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2021-11-06 21:37:42 +0100

    Finalisation du visuel

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ var app = express();
 const port = 3000;
 const webpack = require('webpack');
 
-const path = require ("http");
+
+
+const path = require ("path");
 // const server = http.createserver(app);
 
 // respond with "hello world" when a GET request is made to the homepage
+app.use('/src', express.static(__dirname + '/src'));
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.sendFile(path.join(__dirname+'/index.html'));
+
 });
 
 var listener = app.listen(port, function(){
---
+++ src/index.js
@@ -0,0 +1,26 @@
+// const $input = document.querySelector("input");
+const $colors = document.querySelectorAll(".color");
+const $red = document.querySelector(".simonContainer .red");
+const $blue = document.querySelector(".simonContainer .blue");
+const $yellow = document.querySelector(".simonContainer .green");
+const $green = document.querySelector(".simonContainer .yellow");
+
+let simonArray = [];
+let count = Number;
+
+$colors.forEach((color) => {
+  color.addEventListener("click", (ev) => {
+    if (ev.target.$blue) {
+      $red.innerHTML = "CLIC !";
+      simonArray.push;
+      console.log(simonArray);
+    }
+
+    document.querySelector("input");
+
+
+   
+  });
+});
+
+
