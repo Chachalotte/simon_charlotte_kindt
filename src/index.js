0c7316fcdeb083d003f5ed1285e875d3f82d6409
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2021-11-06 22:12:12 +0100

    Ajout des loops, systeme de tour et changement du texte en fonction du clic

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const $blue = document.querySelector(".simonContainer .blue");
 const $yellow = document.querySelector(".simonContainer .green");
 const $green = document.querySelector(".simonContainer .yellow");
 
-let simonArray = [];
+let simonArray = []; // Le tableau qui contient toutes les valeurs de la séquence à taper dans l'ordre
 let count = Number;
+let tour = 1;
+let simonValue; // La valeur du bouton qui est joué. On l'insère dans le tableau
 
 $colors.forEach((color) => {
   color.addEventListener("click", (ev) => {
@@ -17,10 +19,30 @@ $colors.forEach((color) => {
     }
 
     document.querySelector("input");
-
-
    
   });
 });
+
+function startGame(){
+
+    switch(tour) {
+        case 1 :
+            simonValue = Math.floor(Math.random() * 4);
+            simonArray.push(simonValue);
+            break;
+
+        case 2 :
+            simonValue = Math.floor(Math.random() * 4);
+            simonArray.push(simonValue);
+            simonValue = Math.floor(Math.random() * 4);
+            simonArray.push(simonValue);
+            break;
+    }
+    tour++;
+   
+    console.log(tour);
+
+    console.log(simonArray);
+};
 
 
